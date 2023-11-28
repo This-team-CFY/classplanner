89e7081c0ae24f8bbae9b9431d55831c14486c64
Baki Tuncer <98457585+batuncer@users.noreply.github.com>
2023-11-28 17:12:03 +0000

    Update index.js
    we dont need swapper

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router, Routes, Switch } from "react-router-dom";
-import "swiper/swiper-bundle.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
