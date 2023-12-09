ba31be544e496b5d047521ecd7ead58794383ee3
AnnaFYZ <113105718+AnnaFYZ@users.noreply.github.com>
2023-12-09 01:19:27 +0000

    calendar

--- front-end/src/components/lessonForm/sessionform.js
+++ front-end/src/components/lessonForm/sessionform.js
@@ -4,27 +4,43 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import axios from '../../utils/axios';
 
-// id SERIAL PRIMARY KEY,
-//     date TIMESTAMP,
-//     time_start TIMESTAMP,
-//     time_end TIMESTAMP,
-//     event_type VARCHAR(250),  -- e.g. Technical Education/Personal Development
-//     location VARCHAR(250),  -- can also be Zoom link
-//     lesson_content_id INT REFERENCES lesson_content(id),  -- derive name of session from this+cohort
-//     cohort_id INT REFERENCES cohort(id)
-
-
 export default function SessionForm() {
+  const [sessionData, setSessionData] = useState([{}]);
+  const [cohortData, setCohortData] = useState([{}]);
+  const [lessonData, setLessonData] = useState([{}])
   const [event, setEvent] = useState(["Saturday Session"]);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
   const [location, setLocation] = useState("");
   const [cohort, setCohort] = useState([]);
   const [description, setDescription] = useState("");
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(null);
-  const [lessons, setLessons] = useState([]);
-  const [cohorts, setCohorts] = useState([]);
-  const [selectedCohort, setSelectedCohort] = useState(null);
-  const [selectedLesson, setSelectedLesson] = useState(null);
+  const [summary, setSummary] = useState("");
+  
+  const handleSelectEvent = (selectedValue) => {
+    setEvent(selectedValue);
+  };
+
+  const handleSelectLocation = (selectedValue) => {
+    setLocation(selectedValue);
+  };  
+
+  const handleSelectCohort = (selectedValue) => {
+    let cohortId = cohortData.find(item => item.name === selectedValue);
+    setCohort(cohortId ? cohort.id: 1);
+  };  
+
+  const handleSelectDescription = (selectedValue) => {
+    let lesson = lessonData.find(
+      item => (
+        selectedValue.includes(item.module) &&
+        selectedValue.includes(item.module_week) &&
+        selectedValue.includes(item.syllabus_link)
+    ));
+    
+    setDescription(lesson ? lesson.id : null);
+    setSummary(selectedValue);
+    console.log(selectedValue)
+  };  
 
   useEffect(() => {
     async function fetchData() {
@@ -32,35 +48,10 @@ export default function SessionForm() {
         const response = await axios.get("session");
         const cohorts = await axios.get("cohort");
         const lessons = await axios.get("lesson_content");
-        setCohorts(cohorts.data.map(
-          item => ({name: item.name, onselect: () => setSelectedCohort(item.id)})
-        ));
-        setLessons(lessons.data);
-        setEvent(response.data.map((item) => item.event_type));
-        setLocation(response.data.map((item) => item.location));
-        //setCohort(cohorts.data.map((item) => item.name));
-        setDescription(
-          lessons.data.map(
-            (item) =>
-              `Module: ${item.module},\nWeek: ${item.module_week},\n link: ${item.syllabus_link}`
-          )
-        );
-  //      [
-//   {
-//     id: 2,
-//     date: "2023-12-05T23:02:48.797Z",
-//     time_start: "2023-12-05T23:02:48.797Z",
-//     time_end: "2023-12-05T23:02:48.797Z",
-//     who_leading: "Barath",
-//     cohort: "London 10",
-//     city: "London",
-//     location: "London",
-//     module_name: "Databases",
-//     module_week: 1,
-//     syllabus_link: "codeyourfuture.com",
-//   },
-// ];
-      } catch (error) {
+        setSessionData(response.data);
+        setCohortData(cohorts.data);
+        setLessonData(lessons.data);
+    } catch (error) {
         console.log(error);
       }
     }
@@ -69,27 +60,31 @@ export default function SessionForm() {
 
   async function submitForm() {
     
+    let sessionInfo = {
+      date: JSON.stringify(startDate).split("T")[0],
+      time_start: JSON.stringify(startDate).split("T")[1].split(".")[0],
+      start_time: startDate,
+      end_time: endDate,
+      time_end: JSON.stringify(endDate).split("T")[1].split(".")[0],
+      cohort_id: cohort,
+      location: location,
+      event_type: event,
+      lesson_content_id: description,
+      summary: summary,
+    };
+    
     try {
-      const response = await axios.post("/session", {
-        date: startDate.split("T")[0],
-        time_start: startDate.split("T")[1].split(".")[0],
-        time_end: endDate.split("T")[1].split(".")[0],
-        cohort_id: selectedCohort,
-        location: location,
-        event_type: event,
-        lesson_content_id: selectedLesson,
-
-      });
+      const response = await axios.post("/session", sessionInfo);
+    } catch (error) {
+      console.log(error);
+    }
+    try {
+      const calendarEvent = await axios.post("create-event", sessionInfo);
     } catch (error) {
       console.log(error);
     }
     
   }
-
-  //const [inputValue, setInputValue] = useState("Saturday Session");
-  // const handleInputChange = (e) => {
-  //   setInputValue(e.target.value);
-  // };
 
   return (
     <div className="formDiv">
@@ -98,7 +93,12 @@ export default function SessionForm() {
         <label className="form-label">
           {" "}
           Event<br></br>
-          <EditableField name="event" type="text" options={event} />
+          <EditableField
+            name="event"
+            type="text"
+            options={sessionData.map((item) => item.event_type)}
+            onSelectChange={handleSelectEvent}
+          />
         </label>
         <label className="form-label">
           Date
@@ -125,18 +125,36 @@ export default function SessionForm() {
         <label className="form-label">
           {" "}
           Location<br></br>
-          <EditableField name="location" type="text" options={location} />
+          <EditableField
+            name="location"
+            type="text"
+            options={sessionData.map((item) => item.location)}
+            onSelectChange={handleSelectLocation}
+          />
         </label>
         <label className="form-label">
           {" "}
           Cohort<br></br>
-          <EditableField name="cohort" type="text" options={cohort} />
+          <EditableField
+            name="cohort"
+            type="text"
+            options={cohortData.map((item) => item.name)}
+            onSelectChange={handleSelectCohort}
+          />
         </label>
         <label className="form-label">
           {" "}
           Lesson Description
           <br></br>
-          <EditableField name="description" type="text" options={description} />
+          <EditableField
+            name="description"
+            type="text"
+            options={lessonData.map(
+              (item) =>
+                `Module: ${item.module},\nWeek: ${item.week_no},\n link: ${item.syllabus_link}`
+            )}
+            onSelectChange={handleSelectDescription}
+          />
         </label>
       </form>
       <button type="submit" onClick={submitForm}>
